760bf91eceea77236bc3177ab89d7a8a2478d664
你瞅啥 <1005052477@qq.com>
2020-06-21 20:23:10 +0800

    解决了三个小问题  么？

--- assets/js/login.js
+++ assets/js/login.js
@@ -7,15 +7,45 @@ $(function () {
         $('.reg-box').hide()
         $('.login-box').show()
     })
-    $('#btn-reg').on('click', function (e) {
-        console.log('ok')
-        if ($('#reg-password').val() !== $('#reg-repassword').val()) {
-            return alert('小老弟，你这俩密码不太对啊')
-        }
-    })
     var form = layui.form
     form.verify({
         pwd: [/^[\S]{6,12}$/
-            , '密码必须6到12位，且不能出现空格']
+            , '密码必须6到12位，且不能出现空格'],
+        repwd: function (value) {
+            var pwd = $('#reg-password').val()
+            if (pwd !== value) {
+                return '小老弟，你这俩密码不太对得上啊'
+            }
+        }
     })
-})                                   
+    $('#form_reg').on('submit', function (e) {
+        e.preventDefault()
+        var data = {
+            username: $('#form-reg[name=username]'),
+            password: $('#form-reg[name=password]'),
+        }
+        $.post('url', data, function (res) {
+            if (res.status !== 0) {
+                return layer.msg(res.message)
+            }
+            layer.msg('注册成功，请登录！')
+            $('#link-login').click()
+        })
+    })
+    $('#form-login').on('click', function (e) {
+        e.preventDefault()
+        $.ajax({
+            url: 'asd',
+            method: 'POST',
+            data: $(this).serialize(),
+            success: function () {
+                if (res.status !== 0) {
+                    return layer.msg('登录失败！')
+                }
+                layer.msg('登录成功！')
+                localStorage.setItem('token', res.token)
+                location.href = './index.html'
+            }
+        })
+    })
+})
