5c9d05e01ed49f62752c6fd3fcf7c1ab5a9e34dc
你瞅啥 <1005052477@qq.com>
2020-06-23 14:30:08 +0800

    端午前夕 好歹是赶上进度了

---
+++ assets/js/index.js
@@ -0,0 +1,39 @@
+$(function () {
+    function getUserInfo() {
+        $.ajax({
+            method: 'GET',
+            url: '/my/userinfo',
+
+            success: function (res) {
+                if (res.status !== 0) {
+                    return '获取用户信息失败'
+                }
+                console.log(res);
+                renderAvatar(res.data)
+            }
+        })
+    }
+    getUserInfo()
+    function renderAvatar(user) {
+        var name = user.username
+        $('#welcome').html('欢迎&nbsp;&nbsp;' + name)
+        if (user.user_pic !== null) {
+            $('.layui-nav-img')
+                .attr('src', user.user_pic)
+                .show()
+            $('.text-avatar').hide()
+        } else {
+            $('.layui-nav-img').hide()
+            var first = name[0].toUpperCase()
+            $('.text-avatar').html(first).show()
+        }
+    }
+    var layer = layui.layer
+    $('#btnLogout').on('click', function () {
+        layer.confirm('确定退出登录?', { icon: 3, title: '提示' }, function (index) {
+            localStorage.removeItem('token')
+            location.href = '/login.html'
+            layer.close(index)
+        })
+    })
+})
--- assets/js/login.js
+++ assets/js/login.js
@@ -8,6 +8,7 @@ $(function () {
         $('.login-box').show()
     })
     var form = layui.form
+    var layer = layui.layer
     form.verify({
         pwd: [/^[\S]{6,12}$/
             , '密码必须6到12位，且不能出现空格'],
@@ -18,27 +19,30 @@ $(function () {
             }
         }
     })
-    $('#form_reg').on('submit', function (e) {
+    // 注册
+    $('#form-reg').on('submit', function (e) {
         e.preventDefault()
         var data = {
-            username: $('#form-reg[name=username]'),
-            password: $('#form-reg[name=password]'),
+            username: $('#form-reg [name=username]').val(),
+            password: $('#form-reg [name=password]').val(),
         }
-        $.post('url', data, function (res) {
+        $.post('/api/reguser', data, function (res) {
             if (res.status !== 0) {
                 return layer.msg(res.message)
             }
+
             layer.msg('注册成功，请登录！')
-            $('#link-login').click()
+            // $('#link-login').click()
         })
     })
-    $('#form-login').on('click', function (e) {
+    // 登录
+    $('#form-login').on('submit', function (e) {
         e.preventDefault()
         $.ajax({
-            url: 'asd',
+            url: '/api/login',
             method: 'POST',
             data: $(this).serialize(),
-            success: function () {
+            success: function (res) {
                 if (res.status !== 0) {
                     return layer.msg('登录失败！')
                 }
